1557b06c571a7bc2d667e173b016bf2b0a5796ad
xiaolizhi1990 <278905751@qq.com>
2021-04-12 10:45:17 +0800

    4-12日提交

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+//  reset CSS
+import "normalize.css/normalize.css";
 
-createApp(App).mount('#app')
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "./assets/iconfont/iconfont.css"
+import '@/styles/index.scss' // global css
+
+import '@/permission' // permission control
+
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount("#app");
