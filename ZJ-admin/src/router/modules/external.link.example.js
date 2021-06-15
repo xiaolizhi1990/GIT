011f4eab11f46a0fd43e2104ac5932be05fdad38
xiaolizhi1990 <278905751@qq.com>
2021-06-15 16:02:43 +0800

    原始模板

---
+++ ZJ-admin/src/router/modules/external.link.example.js
@@ -0,0 +1,9 @@
+
+export default {
+    path: 'https://hooray.github.io/fantastic-admin',
+    name: 'externalLinkExample',
+    meta: {
+        title: '官网',
+        icon: 'sidebar-external-link'
+    }
+}
